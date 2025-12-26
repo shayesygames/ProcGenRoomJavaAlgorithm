20c23ae42ec8f68b5f944e43638ac3521c6d84ef
shayesygames <shayesygames@gmail.com>
2025-12-26 10:24:34 -0600

    Initial commit, make sure remote repo works

---
+++ src/main/Node.java
@@ -0,0 +1,115 @@
+package Main.BST;
+
+public class Node {
+
+    RoomInfo roomInfo;
+    Node leftNode;
+    Node rightNode;
+    Node connectionNode1;
+    Node connectionNode2;
+
+    public Node(RoomInfo roomInfo) {
+        this.roomInfo = roomInfo;
+    }
+
+    public Node getConnectionNode1() {
+        return connectionNode1;
+    }
+
+    public Node getConnectionNode2() {
+        return connectionNode2;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public Node getRightNode() {
+        return rightNode;
+    }
+
+    public void setConnectionNode1(Node connectionNode) {
+        this.connectionNode1 = connectionNode;
+    }
+
+    public void setConnectionNode2(Node connectionNode) {
+        this.connectionNode2 = connectionNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public RoomInfo getRoomInfo() {
+        return roomInfo;
+    }
+
+    public void setRoomInfo(RoomInfo roomInfo) {
+        this.roomInfo = roomInfo;
+    }
+
+    //connect new node to this node to available node
+    public void addConnectingRoom(Node nodeToConnect) {
+        if (connectionNode1 != null && connectionNode2 != null) {
+            System.out.println("No connections available");
+            return;
+        }
+
+        if (connectionNode1 == null) {
+            this.setConnectionNode1(nodeToConnect);
+        } else {
+            this.setConnectionNode2(nodeToConnect);
+        }
+    }
+
+    //check if nodes are already connected
+    public boolean isAlreadyConnected(Node nodeToConnect) {
+        return (this.getConnectionNode1() != null && this.getConnectionNode1().equals(nodeToConnect))
+                || (this.getConnectionNode2() != null && this.getConnectionNode2().equals(nodeToConnect));
+    }
+
+    //check for node equivalence based on room id
+    public boolean equals(Node candidateNode) {
+        RoomInfo thisRoom = this.roomInfo;
+        RoomInfo candidateRoom = candidateNode.getRoomInfo();
+        return thisRoom.roomId == candidateRoom.roomId;
+    }
+
+    //prints unorganized list of rooms
+    public void printRoomInfo() {
+        RoomInfo roomInfo = this.getRoomInfo();
+        System.out.println("Room info: ");
+        System.out.println("Room id: " + roomInfo.getRoomId());
+        System.out.println("origin : (" + roomInfo.getOriginX() + ", " + roomInfo.getOriginY() + ")");
+        System.out.println("Height x Witdth (" + roomInfo.getHeight() + ", " + roomInfo.getWidth() + ")");
+
+        if (connectionNode1 != null || connectionNode2 != null) {
+            System.out.println("connections: " + getRoomIdFromConnection());
+        }
+    }
+
+    //get rooms connected to this node
+    public String getRoomIdFromConnection() {
+        String result = "";
+        if (connectionNode1 != null) {
+            result = result + connectionNode1.getRoomInfo().getRoomId() + ", ";
+        }
+        if (connectionNode2 != null) {
+            result = result + connectionNode2.getRoomInfo().getRoomId();
+        }
+        return result;
+    }
+
+    //check if there are rooms available
+    public boolean hasFreeRoom() {
+        boolean hasFreeRooms = connectionNode1 == null || connectionNode2 == null;
+//        if (!hasFreeRooms) {
+//            System.out.println("connection cannot be made, no free Rooms");
+//        }
+        return hasFreeRooms;
+    }
+}
